668c8332d830a2a075c908da8659badb57a6e329
Antriksh Chaudhary <antdeutsch19@gmail.com>
2023-10-31 16:48:45 +0100

    trying to push on github

--- src/raytracer/shade/MyPhong.java
+++ src/raytracer/shade/MyPhong.java
@@ -1,3 +1,78 @@
+// package raytracer.shade;
+
+// import raytracer.core.Hit;
+// import raytracer.core.LightSource;
+// import raytracer.core.Shader;
+// import raytracer.core.Trace;
+// import raytracer.math.Color;
+// import raytracer.math.Vec3;
+
+// public class MyPhong implements Shader {
+
+//   private final Shader Myinner;
+//   private final Color ambient;
+//   private final float diffuse;
+//   private final float specular;
+//   private final float shininess;
+
+//   public MyPhong(
+//     final Shader inner,
+//     final Color ambientColor,
+//     final float diffuseConstant,
+//     final float specularConstant,
+//     final float glossFactor
+//   ) {
+//     this.Myinner = inner;
+//     this.ambient = ambientColor;
+//     this.diffuse = diffuseConstant;
+//     this.specular = specularConstant;
+//     this.shininess = glossFactor;
+//   }
+
+//   @Override
+//   public Color shade(final Hit hit, final Trace trace) {
+//     // surface colour nikala hit position par
+//     Color surfaceColor = Myinner.shade(hit, trace);
+//     // ye ambient light hai
+//     Color ambientColor = ambient.mul(surfaceColor);
+
+//     Color diffuseColor = Color.BLACK;
+//     Vec3 normal = hit.getNormal().normalized();
+//     Vec3 viewDirection = trace.getRay().dir().neg().normalized();
+//     for (LightSource lightSource : trace.getScene().getLightSources()) {
+//       Vec3 lightDirection = lightSource
+//         .getLocation()
+//         .sub(hit.getPoint())
+//         .normalized();
+//       float intensity = Math.max(0, normal.dot(lightDirection));
+//       diffuseColor =
+//         diffuseColor.add(
+//           lightSource.getColor().mul(surfaceColor).scale(intensity)
+//         );
+//     }
+//     diffuseColor = diffuseColor.scale(diffuse);
+
+//     Color specularColor = Color.BLACK;
+//     for (LightSource lightSource : trace.getScene().getLightSources()) {
+//       Vec3 lightDirection = lightSource
+//         .getLocation()
+//         .sub(hit.getPoint())
+//         .normalized();
+//       Vec3 reflectionDirection = lightDirection.neg().reflect(normal);
+
+//       float intensity = (float) Math.pow(
+//         Math.max(0, reflectionDirection.dot(viewDirection)),
+//         shininess
+//       );
+//       specularColor =
+//         specularColor.add(lightSource.getColor().scale(intensity));
+//     }
+//     specularColor = specularColor.scale(specular);
+
+//     //final phong shading jo hui uska result
+//     return ambientColor.add(diffuseColor).add(specularColor);
+//   }
+// }
 package raytracer.shade;
 
 import raytracer.core.Hit;
@@ -31,9 +106,7 @@ public class MyPhong implements Shader {
 
   @Override
   public Color shade(final Hit hit, final Trace trace) {
-    // surface colour nikala hit position par
     Color surfaceColor = Myinner.shade(hit, trace);
-    // ye ambient light hai
     Color ambientColor = ambient.mul(surfaceColor);
 
     Color diffuseColor = Color.BLACK;
@@ -63,12 +136,12 @@ public class MyPhong implements Shader {
         Math.max(0, reflectionDirection.dot(viewDirection)),
         shininess
       );
+      // Use light source color in the specular term as well
       specularColor =
         specularColor.add(lightSource.getColor().scale(intensity));
     }
     specularColor = specularColor.scale(specular);
 
-    //final phong shading jo hui uska result
     return ambientColor.add(diffuseColor).add(specularColor);
   }
 }
